34bdc7b5e5ed5622ee2a5dc39b4eb1c9d0369002
Barbara <Barbara@L-009-0282.sg.hcare.ch>
2021-03-25 13:52:12 +0100

    add workspace

--- ellie/ellie/abmeldung.php
+++ ellie/ellie/abmeldung.php
@@ -3,4 +3,7 @@
   unset($_SESSION['benutzer']) ;
   unset($_SESSION['password']);
   echo "Benutzer abgemeldet.";
+  $html_Output = "<a href=../index.php>Startseite</a>";
+  echo $html_Output;
 ?>
+
